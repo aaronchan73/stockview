7bd1741e1cca08d6bf6fc7d0b5a2c5120f50d370
Aaron <aaronc73@students.cs.ubc.ca>
2021-12-30 17:04:47 -0800

    Added useEffect to prevent infinite fetches.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { TiDelete } from "react-icons/ti";
 
 let values = [];
@@ -11,32 +11,39 @@ const Stock = ({ stock, onDelete }) => {
     let API_URL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${CODE}&apikey=${API_KEY}`;
     const [open, setOpen] = useState(0);
 
-    fetch(API_URL)
-        .then(
-            (response) => {
-                return response.json();
-            }
-        )
-        .then(
-            (data) => {
+    useEffect(() => {
+        fetchAPI();
+    }, []);
 
-                for (var date in data["Time Series (Daily)"]) {
-                    values.push(data["Time Series (Daily)"][date]["1. open"]);
+    const fetchAPI = () => {
+        fetch(API_URL)
+            .then(
+                (response) => {
+                    return response.json();
                 }
+            )
+            .then(
+                (data) => {
 
-                console.log(values);
-                setOpen(values[0]);
-            }
-        )
+                    for (var date in data["Time Series (Daily)"]) {
+                        values.push(data["Time Series (Daily)"][date]["1. open"]);
+                    }
+
+                    console.log(values);
+                    setOpen(values[0]);
+                }
+            )
+
+    }
 
     return (
         <div className="stock">
             <h4>Stock: {stock.name} <TiDelete onClick={() => onDelete(stock.id)} /> </h4>
             <p>Bought Price: ${stock.bought}</p>
             <p>Open Price: ${(open * 1).toFixed(2)}</p>
-            {(open - stock.bought) >= 0 ? 
-            <p style={{color: "#00ff4c"}}>Net Profit: ${(open - stock.bought).toFixed(2)}</p> :
-            <p style={{color: "#ff2e2e"}}>Net Profit: ${(open - stock.bought).toFixed(2)}</p>}
+            {(open - stock.bought) >= 0 ?
+                <p style={{ color: "#00ff4c" }}>Net Profit: ${(open - stock.bought).toFixed(2)}</p> :
+                <p style={{ color: "#ff2e2e" }}>Net Profit: ${(open - stock.bought).toFixed(2)}</p>}
         </div>
     );
 }
