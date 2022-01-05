35543fd5d31fe015b98e5f696714bc048fadb293
Aaron <aaronc73@students.cs.ubc.ca>
2022-01-04 16:36:06 -0800

    Updates

--- src/components/Button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Button = ({title, onClick}) => {
-    return <button className="btn" onClick={onClick}>{title}</button>
-}
-
-export default Button;
--- src/components/TotalProfits.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const TotalProfits = ({profits}) => {
-    return <h3>Total Net Profit: ${profits.toFixed(2)}</h3>
-}
-
-export default TotalProfits
